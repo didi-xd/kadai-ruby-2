c04cb25ddf649202185f4289eebf731e6615158e
didi <dd7313688@gmail.com>
2022-03-09 06:18:47 +0000

    fizzbuzz commit

---
+++ kadai-fizzbuzz.rb
@@ -0,0 +1,22 @@
+def fizzbuzz(n)
+  if n % 15 == 0
+      puts "FizzBuzz"
+  elsif n % 3 == 0
+      puts "Fizz"
+  elsif n % 5 == 0
+      puts "Buzz"
+  else 
+      puts n
+  end
+end
+num_max = 100
+(1..num_max).each do |n|
+  puts fizzbuzz(n)
+end
+
+
+# 以下は動作確認用の記述です
+#puts fizzbuzz(1)
+#puts fizzbuzz(3)
+#puts fizzbuzz(5)
+#puts fizzbuzz(15)
